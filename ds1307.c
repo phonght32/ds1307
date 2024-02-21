38d2619a9641c812912ca8f2f79e523a82203754
phonght32 <thanhphongho1998@gmail.com>
2024-02-21 22:48:57 +0700

    update ds1307

---
+++ ds1307.c
@@ -0,0 +1,164 @@
+#include "stdlib.h"
+#include "string.h"
+#include "ds1307.h"
+
+#define DS1307_TIME_REG_ADDR    	0
+#define DS1307_CONTROL_REG_ADDR 	7
+
+#define CH_BIT      				(1 << 7)
+#define HOUR12_BIT  				(1 << 6)
+#define PM_BIT      				(1 << 5)
+#define SQWE_BIT    				(1 << 4)
+#define OUT_BIT     				(1 << 7)
+
+#define CH_MASK      				0x7f
+#define SECONDS_MASK 				0x7f
+#define HOUR12_MASK  				0x1f
+#define HOUR24_MASK  				0x3f
+#define SQWEF_MASK   				0x03
+#define SQWE_MASK    				0xef
+#define OUT_MASK     				0x7
+
+typedef struct ds1307 {
+	ds1307_func_i2c_send  			i2c_send; 		/*!< I2C send function */
+	ds1307_func_i2c_recv			i2c_recv;		/*!< I2C receive function */
+} ds1307_t;
+
+static uint8_t bcd2dec(uint8_t val)
+{
+	return (val >> 4) * 10 + (val & 0x0f);
+}
+
+static uint8_t dec2bcd(uint8_t val)
+{
+	return ((val / 10) << 4) + (val % 10);
+}
+
+static err_code_t ds1307_i2c_write_reg(ds1307_handle_t handle, uint8_t reg_addr, uint8_t *buf, uint16_t len)
+{
+	uint8_t buf_send[len + 1];
+
+	buf_send[0] = reg_addr;
+	memcpy(&buf_send[1], buf, len);
+	handle->i2c_send(buf_send, len + 1);
+
+	return ERR_CODE_SUCCESS;
+}
+
+static err_code_t ds1307_i2c_read_reg(ds1307_handle_t handle, uint8_t reg_addr, uint8_t *buf, uint16_t len)
+{
+	uint8_t buffer[1];
+
+	buffer[0] = reg_addr | 0x80;
+	handle->i2c_send(buffer, 1);
+	handle->i2c_recv(buf, len);
+
+	return ERR_CODE_SUCCESS;
+}
+
+ds1307_handle_t ds1307_init(void)
+{
+	ds1307_handle_t handle = calloc(1, sizeof(ds1307_t));
+	if (handle == NULL)
+	{
+		return NULL;
+	}
+
+	return handle;
+}
+
+err_code_t ds1307_set_config(ds1307_handle_t handle, ds1307_cfg_t config)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	handle->i2c_send = config.i2c_send;
+	handle->i2c_recv = config.i2c_recv;
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t ds1307_config(ds1307_handle_t handle)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	/* Nothing to do */
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t ds1307_get_time(ds1307_handle_t handle, struct tm *time)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	err_code_t ret;
+	uint8_t buf[7];
+
+	ret = ds1307_i2c_read_reg(handle, DS1307_TIME_REG_ADDR, buf, 7);
+	if (ret != ERR_CODE_SUCCESS)
+	{
+		return ERR_CODE_FAIL;
+	}
+
+	time->tm_sec = bcd2dec(buf[0] & SECONDS_MASK);
+	time->tm_min = bcd2dec(buf[1]);
+
+	if (buf[2] & HOUR12_BIT)
+	{
+		// RTC in 12-hour mode
+		time->tm_hour = bcd2dec(buf[2] & HOUR12_MASK) - 1;
+		if (buf[2] & PM_BIT)
+			time->tm_hour += 12;
+	}
+	else
+	{
+		time->tm_hour = bcd2dec(buf[2] & HOUR24_MASK);
+	}
+
+	time->tm_wday = bcd2dec(buf[3]) - 1;
+	time->tm_mday = bcd2dec(buf[4]);
+	time->tm_mon  = bcd2dec(buf[5]) - 1;
+	time->tm_year = bcd2dec(buf[6]) + 2000;
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t ds1307_set_time(ds1307_handle_t handle, struct tm time)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	err_code_t ret;
+
+	uint8_t buf[7] = {
+		dec2bcd(time.tm_sec),
+		dec2bcd(time.tm_min),
+		dec2bcd(time.tm_hour),
+		dec2bcd(time.tm_wday + 1),
+		dec2bcd(time.tm_mday),
+		dec2bcd(time.tm_mon + 1),
+		dec2bcd(time.tm_year - 2000)
+	};
+
+	ret = ds1307_i2c_write_reg(handle, DS1307_TIME_REG_ADDR, buf, sizeof(buf));
+	if (ret != ERR_CODE_SUCCESS)
+	{
+		return ERR_CODE_FAIL;
+	}
+
+	return ERR_CODE_SUCCESS;
+}
---
+++ ds1307.h
@@ -0,0 +1,113 @@
+// MIT License
+
+// Copyright (c) 2024 phonght32
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef __DS1307_H__
+#define __DS1307_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "time.h"
+#include "err_code.h"
+
+typedef err_code_t (*ds1307_func_i2c_send)(uint8_t *buf_send, uint16_t len);
+typedef err_code_t (*ds1307_func_i2c_recv)(uint8_t *buf_recv, uint16_t len);
+
+/**
+ * @brief   Handle structure.
+ */
+typedef struct ds1307 *ds1307_handle_t;
+
+/**
+ * @brief   Configuration structure.
+ */
+typedef struct {
+	ds1307_func_i2c_send  			i2c_send; 		/*!< I2C send function */
+	ds1307_func_i2c_recv			i2c_recv;		/*!< I2C receive function */
+} ds1307_cfg_t;
+
+/*
+ * @brief   Initialize DS1307 with default parameters.
+ *
+ * @note    This function must be called first.
+ *
+ * @param   None.
+ *
+ * @return
+ *      - Handle structure: Success.
+ *      - Others:           Fail.
+ */
+ds1307_handle_t ds1307_init(void);
+
+/*
+ * @brief   Set configuration parameters.
+ *
+ * @param 	handle Handle structure.
+ * @param   config Configuration structure.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t ds1307_set_config(ds1307_handle_t handle, ds1307_cfg_t config);
+
+/*
+ * @brief   Configure DS1307 to run.
+ *
+ * @param 	handle Handle structure.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t ds1307_config(ds1307_handle_t handle);
+
+/**
+ * @brief   Get time.
+ *
+ * @param 	handle Handle structure.
+ * @param   time Pointer references to the struct tm.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t ds1307_get_time(ds1307_handle_t handle, struct tm *time);
+
+/**
+ * @brief   Set time.
+ *
+ * @param 	handle Handle structure.
+ * @param   time Struct tm.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t ds1307_set_time(ds1307_handle_t handle, struct tm time);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __DS1307_H__ */
